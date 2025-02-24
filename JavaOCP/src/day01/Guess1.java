7853ea93468c443d981f4080fb7836081e37e3b5
teacher <teacher@DESKTOP-TDDQARH>
2025-02-24 19:49:42 +0800

    java

---
+++ JavaOCP/src/day01/Guess1.java
@@ -0,0 +1,28 @@
+package day01;
+
+import java.util.Scanner;
+
+public class Guess1 {
+
+	public static void main(String[] args) {
+		// 1~9 猜數字
+		int ans = 7;
+		do {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("請輸入數字: ");
+			int userGuess = sc.nextInt();
+			// 判斷
+			if(userGuess > ans) {
+				System.out.println("猜大了");
+			} else if(userGuess < ans) {
+				System.out.println("猜小了");
+			} else {
+				System.out.println("答對了");
+				break;
+			}
+		} while(true);
+		
+
+	}
+
+}
