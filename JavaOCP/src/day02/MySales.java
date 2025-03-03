d190a8151664e104cf0802a7cd44962b481a3f7e
teacher <teacher@DESKTOP-TDDQARH>
2025-03-03 20:25:42 +0800

    web

--- JavaOCP/src/day02/MySales.java
+++ JavaOCP/src/day02/MySales.java
@@ -26,12 +26,15 @@ public class MySales {
 		System.out.println(cost2);
 		// -----------------------------------------------------
 		int qty3 = 15;
+		int cost3 = buyFiveGetTwoFree(qty3);
+		System.out.println(cost3);
 	}
 	
 	// 買五送二
 	public static int buyFiveGetTwoFree(int qty) {
-		
-		return 0;
+		int currentQty = (qty / 7)*5 + qty % 7;
+		int cost = currentQty * price;
+		return cost;
 	}
 	
 	// 買一送一
