4281c997935e8d3197c91006b30ad144e456225d
teacher <teacher@DESKTOP-TDDQARH>
2025-03-03 20:47:44 +0800

    j

--- JavaOCP/src/day02/MyBank.java
+++ JavaOCP/src/day02/MyBank.java
@@ -20,16 +20,26 @@ public class MyBank {
 	
 	public static void deposit(int amount) {
 		balance += amount;
-		System.out.printf("存款 $%d%n", amount);
+		System.out.printf("存款 $%,d%n", amount);
 	}
 	
 	public static void withdraw(int amount) {
+		// 提款金額 > 0 && 提款金額 <= 目前餘額
+		// 所以若 提款金額 <= 0 或 提款金額 > 目前餘額 都不能進行後續的提款動作
+		if(amount <= 0) {
+			System.out.printf("提款金額 $%,d 提款金額必須 > $0%n", amount);
+			return; // 提前結束方法
+		} else if (amount > balance) {
+			System.out.printf("提款金額 $%,d > 目前餘額 $%,d%n", amount, balance);
+			return; // 提前結束方法
+		}
+		
 		balance -= amount;
-		System.out.printf("提款 $%d%n", amount);
+		System.out.printf("提款 $%,d%n", amount);
 	}
 	
 	public static void printBalance() {
-		System.out.printf("餘額 $%d%n", balance);
+		System.out.printf("餘額 $%,d%n", balance);
 	}
 
 }
