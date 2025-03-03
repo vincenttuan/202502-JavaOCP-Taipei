c0eaf5cf1da7f0e9d6b825c6860bc4f6d21ad858
teacher <teacher@DESKTOP-TDDQARH>
2025-03-03 19:06:50 +0800

    web

---
+++ JavaOCP/src/day02/MyBMI2.java
@@ -0,0 +1,18 @@
+package day02;
+
+public class MyBMI2 {
+
+	public static void main(String[] args) {
+		// 第一組身高體重
+		double h1 = 170, w1 = 60;
+		// 第二組身高體重
+		double h2 = 160, w2 = 50;
+		
+	}
+	
+	public static void calcBMI(double h, double w) {
+		double bmi = w / Math.pow(h/100, 2);
+		System.out.println(bmi);
+	}
+
+}
