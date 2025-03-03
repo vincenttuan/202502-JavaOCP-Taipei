acfcbc015cb761129965f9e72e598fb5ff67df9f
teacher <teacher@DESKTOP-TDDQARH>
2025-03-03 21:10:23 +0800

    j

--- JavaOCP/src/day02/MyATM.java
+++ JavaOCP/src/day02/MyATM.java
@@ -28,18 +28,23 @@ public class MyATM {
 	public static void action() {
 		Scanner sc = new Scanner(System.in);
 		int choice = sc.nextInt(); // 1, 2, 3, 或 0
+		int amount = 0; // 存提款金額
 		switch (choice) {
 			case 1: // Deposit(存款)
-				
+				System.out.print("請輸入存款金額: ");
+				amount = sc.nextInt();
+				MyBank.deposit(amount);
 				break;
 			case 2: // Withdraw(提款)
-				
+				System.out.print("請輸入提款金額: ");
+				amount = sc.nextInt();
+				MyBank.withdraw(amount);
 				break;
 			case 3: // Print(列印目前餘額)
-				
+				MyBank.printBalance();
 				break;
 			case 0: // Exit(離開)
-				
+				System.exit(0); // 強制離開系統
 				break;	
 			default:
 				System.out.println("選擇錯誤, 請重新選擇!");
