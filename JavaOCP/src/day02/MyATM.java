850bf583fc93e0026d31f8c7358ad7440b9fe3a6
teacher <teacher@DESKTOP-TDDQARH>
2025-03-03 21:00:05 +0800

    j

---
+++ JavaOCP/src/day02/MyATM.java
@@ -0,0 +1,24 @@
+package day02;
+
+public class MyATM {
+
+	public static void main(String[] args) {
+		menu();
+
+	}
+	
+	public static void menu() {
+		System.out.println("+-------------+");
+		System.out.println("|     ATM     |");
+		System.out.println("+-------------+");
+		System.out.println("| 1. Deposit  |");
+		System.out.println("| 2. Withdraw |");
+		System.out.println("| 3. Print    |");
+		System.out.println("| 0. Exit     |");
+		System.out.println("+-------------+");
+		System.out.println("choice => ");
+		
+		
+	}
+	
+}
