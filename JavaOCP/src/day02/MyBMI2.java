d86c3327aae855c325970a8fb61638a808c06365
teacher <teacher@DESKTOP-TDDQARH>
2025-03-03 19:21:33 +0800

    web

--- JavaOCP/src/day02/MyBMI2.java
+++ JavaOCP/src/day02/MyBMI2.java
@@ -11,12 +11,23 @@ public class MyBMI2 {
 		calcBMI(h1, w1);
 		calcBMI(h2, w2);
 		// -----------------------------------------
+		// 第一組 bmi 要印到小數點 2 位
+		// 第二組 bmi 要印出計算的原始資料
+		double bmi1 = getBMI(h1, w1);
+		System.out.printf("%.2f%n", bmi1);
 		
+		double bmi2 = getBMI(h2, w2);
+		System.out.println(bmi2);
+	}
+	
+	public static double getBMI(double h, double w) {
+		double bmi = w / Math.pow(h/100, 2);
+		return bmi; // 將計算好的 bmi 回傳
 	}
 	
 	public static void calcBMI(double h, double w) {
 		double bmi = w / Math.pow(h/100, 2);
 		System.out.println(bmi);
 	}
-
+	
 }
