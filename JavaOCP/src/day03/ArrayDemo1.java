90b0baf6647cc5c5bba3e138c43aa1fc500d004f
teacher <teacher@DESKTOP-TDDQARH>
2025-03-07 20:49:27 +0800

    j

---
+++ JavaOCP/src/day03/ArrayDemo1.java
@@ -0,0 +1,31 @@
+package day03;
+
+public class ArrayDemo1 {
+
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+		//     scores -> [0, 0, 0, 0, 0] 有初始值
+		// 維度 index  ->  0  1  2  3  4 
+		// 長度 length = 5
+		// 最小維度 = 0
+		// 最大維度 = 4 (5-1) 公式: 陣列長度 length - 1
+		// 給定資料 scores[0] = 90;
+		
+		scores[0] = 90;
+		scores[1] = 100;
+		scores[2] = 50;
+		scores[3] = 70;
+		scores[4] = 35;
+		
+		System.out.println(scores);
+		System.out.println(scores.length);
+		System.out.println(scores[0] + ", " + scores[1] + ", " + scores[2] + ", " + scores[3] + ", " + scores[4]);
+		
+		for(int i=0;i<scores.length;i++) {
+			System.out.print(scores[i] + ", ");
+		}
+		System.out.println();
+		
+	}
+
+}
