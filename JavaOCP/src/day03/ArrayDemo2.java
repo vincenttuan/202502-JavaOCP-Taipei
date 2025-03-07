88cc5234dc99f6378a980d473b74e964ffeca05b
teacher <teacher@DESKTOP-TDDQARH>
2025-03-07 21:13:46 +0800

    j

---
+++ JavaOCP/src/day03/ArrayDemo2.java
@@ -0,0 +1,16 @@
+package day03;
+
+import java.util.Arrays;
+
+public class ArrayDemo2 {
+
+	public static void main(String[] args) {
+		// 陣列給予初始值 
+		//int[] scores = new int[] {90, 100, 50, 70, 35};
+		int[] scores = {90, 100, 50, 70, 35};
+		
+		System.out.println(Arrays.toString(scores));
+
+	}
+
+}
