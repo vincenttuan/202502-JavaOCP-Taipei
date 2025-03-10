604c58952297b0c2468d15d48f8948de057df7cb
teacher <teacher@DESKTOP-TDDQARH>
2025-03-10 19:56:14 +0800

    j

---
+++ JavaOCP/src/day04/ArrayDemo10.java
@@ -0,0 +1,10 @@
+package day04;
+
+public class ArrayDemo10 {
+	public static void main(String[] args) {
+		int[] scores1 = {50,  90, 20, 80};
+		int[] scores2 = {75, 99, 45, 65, 95};
+		// 計算全部分數加總為何 ?
+		
+	}
+}
