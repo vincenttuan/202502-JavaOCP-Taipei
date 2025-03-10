5b30996c8a5453fa6874ca96d2b1b3c05fa6ccc2
teacher <teacher@DESKTOP-TDDQARH>
2025-03-10 20:51:00 +0800

    j

--- JavaOCP/src/day04/ArrayDemo10.java
+++ JavaOCP/src/day04/ArrayDemo10.java
@@ -1,10 +1,14 @@
 package day04;
 
+import java.util.stream.IntStream;
+
 public class ArrayDemo10 {
 	public static void main(String[] args) {
 		int[] scores1 = {50,  90, 20, 80};
 		int[] scores2 = {75, 99, 45, 65, 95};
 		// 計算全部分數加總為何 ?
+		int sum = IntStream.concat(IntStream.of(scores1), IntStream.of(scores2)).sum();
+		System.out.println(sum);
 		
 	}
 }
