0ed18664dce958827fe8c633ee1fe6c7b7648f0b
teacher <teacher@DESKTOP-TDDQARH>
2025-03-10 21:23:47 +0800

    j

--- JavaOCP/src/day04/ArrayDemo12.java
+++ JavaOCP/src/day04/ArrayDemo12.java
@@ -7,14 +7,22 @@ public class ArrayDemo12 {
 		// 試問要找多少錢 ? 如何找 ?
 		int totalChange = 100 - 23; // 元
 		int[] coins = {50, 10, 5, 1}; // 硬幣面額
-		int[] count = {0, 0, 0, 0}; // 各硬幣的數量
+		int[] counts = {0, 0, 0, 0}; // 各硬幣的數量
 		
-		
-		
+		//int size = coins.length;
+		for(int i=0, size = coins.length;i<size;i++) {
+			/*
+			int count = totalChange / coins[i]; // 枚
+			counts[i] = count;
+			totalChange = totalChange % coins[i]; // 剩餘
+			*/
+			counts[i] = totalChange / coins[i]; // 枚
+			totalChange %= coins[i]; // 剩餘
+		}
 		
 		System.out.printf("找 %d 元%n", totalChange);
 		for(int i=0;i<coins.length;i++) {
-			System.out.printf("%d 元 %d 枚\n", coins[i], count[i]);
+			System.out.printf("%d 元 %d 枚\n", coins[i], counts[i]);
 		}
 		
 	}
