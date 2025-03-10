41d9e716520a438e2088cb5cf84f1d6f22df5b03
teacher <teacher@DESKTOP-TDDQARH>
2025-03-10 19:26:49 +0800

    j

---
+++ JavaOCP/src/day04/ArrayDemo7.java
@@ -0,0 +1,14 @@
+package day04;
+
+public class ArrayDemo7 {
+	public static void main(String[] args) {
+		int[] scores1 = {50, 90, 20, 80};
+		int[] scores2 = {75, 100, 45, 65};
+		// 請求出最高分 = ?
+		// 答案: 100
+		
+		
+		
+		
+	}
+}
