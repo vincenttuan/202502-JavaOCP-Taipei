6aac41eeebe427c3e768af0f6a3cc7081180063e
teacher <teacher@DESKTOP-TDDQARH>
2025-03-10 19:33:36 +0800

    j

--- JavaOCP/src/day04/ArrayDemo7.java
+++ JavaOCP/src/day04/ArrayDemo7.java
@@ -6,7 +6,22 @@ public class ArrayDemo7 {
 		int[] scores2 = {75, 100, 45, 65};
 		// 請求出最高分 = ?
 		// 答案: 100
+		int max = scores1[0];
 		
+		for(int score : scores1) {
+			if(score > max) {
+				max = score;
+			}
+		}
+		//System.out.println(max);
+		
+		for(int score : scores2) {
+			if(score > max) {
+				max = score;
+			}
+		}
+		
+		System.out.println(max);
 		
 		
 		
