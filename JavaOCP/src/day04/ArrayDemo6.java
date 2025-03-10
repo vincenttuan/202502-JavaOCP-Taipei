986d7ebda4e4f7500904abf27fa3c4b736ee3a94
teacher <teacher@DESKTOP-TDDQARH>
2025-03-10 19:15:58 +0800

    j

--- JavaOCP/src/day04/ArrayDemo6.java
+++ JavaOCP/src/day04/ArrayDemo6.java
@@ -4,6 +4,13 @@ public class ArrayDemo6 {
 	public static void main(String[] args) {
 		int[] scores = {50, 90, 20, 80};
 		// 請印出最高分 = ?
-		
+		int max = scores[0]; // 假設 scores[0] 是最高分
+		for(int i=1;i<scores.length;i++) {
+			if(scores[i] > max) {
+				max = scores[i]; // 替換掉最大值
+			}
+		}
+		System.out.printf("最大值: %d%n", max);
+		// 請印出最低分 = ?
 	}
 }
