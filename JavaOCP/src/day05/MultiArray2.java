4bda13550e91b869c262fc6de4d02bfdb7f0544c
teacher <teacher@DESKTOP-TDDQARH>
2025-03-14 19:33:53 +0800

    j

--- JavaOCP/src/day05/MultiArray2.java
+++ JavaOCP/src/day05/MultiArray2.java
@@ -6,14 +6,15 @@ public class MultiArray2 {
 	public static void main(String[] args) {
 		// 井字遊戲
 		char[][] ttt = {
-				{' ', ' ', ' '}, 
-				{' ', ' ', ' '},
-				{' ', ' ', ' '}
+				{'O', 'O', 'X'}, 
+				{'X', 'O', ' '},
+				{'X', ' ', ' '}
 		};
 		
 		printTTT(ttt);
-		
-		
+		// 請問 O 如何會贏 ?
+		ttt[2][1] = 'O';
+		printTTT(ttt);
 	}
 	
 	// 印出 ttt 的資訊
