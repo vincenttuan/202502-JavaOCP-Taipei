36d6359728a67df730f25cba6e6f1e83665e66b7
teacher <teacher@DESKTOP-TDDQARH>
2025-03-14 19:40:55 +0800

    j

---
+++ JavaOCP/src/day05/MultiArray3.java
@@ -0,0 +1,17 @@
+package day05;
+
+public class MultiArray3 {
+	public static void main(String[] args) {
+		// 5 個學生的 {座號, 身高, 體重} 的資料如下
+		double[][] students = {
+				{1, 170.0, 60.0},
+				{2, 180.0, 90.0},
+				{3, 160.0, 45.0},
+				{4, 175.0, 55.0},
+				{5, 183.0, 75.0},
+		};
+		
+		// 請印出每一個學生的 bmi 值
+		
+	}
+}
