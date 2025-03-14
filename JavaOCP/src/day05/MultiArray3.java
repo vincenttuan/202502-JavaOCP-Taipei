e070c5d10bba8f5d3d3d5a3c67358c96ab3041c2
teacher <teacher@DESKTOP-TDDQARH>
2025-03-14 19:51:00 +0800

    j

--- JavaOCP/src/day05/MultiArray3.java
+++ JavaOCP/src/day05/MultiArray3.java
@@ -14,8 +14,13 @@ public class MultiArray3 {
 		for(double[] student : students) {
 			System.out.printf("座號:%.0f 身高:%.1f 體重:%.1f%n", student[0], student[1], student[2]);
 		}
-		
+		//----------------------------------------------
 		// 2.請計算並印出每一個學生的 bmi 值
-		
+		for(double[] student : students) {
+			double h = student[1]/100;
+			double w = student[2];
+			double bmi = w / (h*h);
+			System.out.printf("座號:%.0f BMI:%.2f%n", student[0], bmi);
+		}
 	}
 }
