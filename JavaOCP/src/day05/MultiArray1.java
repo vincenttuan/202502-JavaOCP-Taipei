bb99d5460a9c7ab8e7f42362c8603e7c60a9108e
teacher <teacher@DESKTOP-TDDQARH>
2025-03-14 19:25:27 +0800

    j

--- JavaOCP/src/day05/MultiArray1.java
+++ JavaOCP/src/day05/MultiArray1.java
@@ -1,5 +1,7 @@
 package day05;
 
+import java.util.Arrays;
+
 public class MultiArray1 {
 
 	public static void main(String[] args) {
@@ -20,12 +22,23 @@ public class MultiArray1 {
 		System.out.println(class1);  // [I@65ae6ba4
 		System.out.println(class2);  // [I@7960847b
 		System.out.println(classes); // [[I@6a6824be
-		// ---------------------------------
+		// ---------------------------------------------------------
 		// classes[列][行]
 		// 取 100
 		System.out.println(classes[0][0]);
 		// 取 90
 		System.out.println(classes[1][2]);
+		// ---------------------------------------------------------
+		System.out.println(Arrays.toString(classes[0]));
+		System.out.println(Arrays.toString(classes[1]));
+		// -- 用 for-loop 印出二維陣列資料 -------------------------------
+		for(int i=0;i<classes.length;i++) {
+			System.out.println(Arrays.toString(classes[i]));
+		}
+		// -- 用 for-each 印出二維陣列資料 -------------------------------
+		for(int[] scores : classes) {
+			System.out.println(Arrays.toString(scores));
+		}
 	}
 
 }
