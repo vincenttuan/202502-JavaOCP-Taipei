0c970952d5fe2a2b6991b112f1a9650c4a0770f0
teacher <teacher@DESKTOP-TDDQARH>
2025-03-17 20:56:34 +0800

    j

---
+++ JavaOCP/src/day06/OO6.java
@@ -0,0 +1,17 @@
+package day06;
+
+import java.util.Arrays;
+
+public class OO6 {
+
+	public static void main(String[] args) {
+		Product p1 = new Product("漢堡", 2, 50);
+		Product p2 = new Product("薯條", 3, 30);
+		Product p3 = new Product("可樂", 5, 20);
+		
+		Product[] products = {p1, p2, p3};
+		System.out.println(Arrays.toString(products));
+
+	}
+
+}
--- JavaOCP/src/day06/Product.java
+++ JavaOCP/src/day06/Product.java
@@ -4,12 +4,18 @@ package day06;
 public class Product {
 	String name; // 品名
 	int qty; // 數量
-	int price; // 價格
+	int price; // 單價
 	
 	public Product(String name, int qty, int price) {
 		this.name = name;
 		this.qty = qty;
 		this.price = price;
 	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", qty=" + qty + ", price=" + price + "]";
+	}
+	
 	
 }
