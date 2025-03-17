08cefe8491f4607ca4e42768af6a295e908f1830
teacher <teacher@DESKTOP-TDDQARH>
2025-03-17 20:59:07 +0800

    j

--- JavaOCP/src/day06/OO6.java
+++ JavaOCP/src/day06/OO6.java
@@ -7,12 +7,14 @@ public class OO6 {
 	public static void main(String[] args) {
 		Product p1 = new Product("漢堡", 2, 50);
 		Product p2 = new Product("薯條", 3, 30);
-		Product p3 = new Product("可樂", 5, 20);
+		Product p3 = new Product("可樂", 5, 15);
 		
 		Product[] products = {p1, p2, p3};
 		System.out.println(products);
 		System.out.println(Arrays.toString(products));
-
+		
+		// 請問總花費 ?
+		
 	}
 
 }
