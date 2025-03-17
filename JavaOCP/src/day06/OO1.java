fc2d18b54a507225e9cb43246099d545c04c44b9
teacher <teacher@DESKTOP-TDDQARH>
2025-03-17 18:55:56 +0800

    j

---
+++ JavaOCP/src/day06/OO1.java
@@ -0,0 +1,18 @@
+package day06;
+
+public class OO1 {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.name = "John";
+		s1.score = 70;
+		
+		Student s2 = new Student();
+		s2.name = "Mary";
+		s2.score = 80;
+		
+		
+
+	}
+
+}
---
+++ JavaOCP/src/day06/Student.java
@@ -0,0 +1,6 @@
+package day06;
+
+public class Student {
+	String name;
+	int score;
+}
