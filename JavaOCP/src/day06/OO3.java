c4af53c826dbcc63a242d80b9d703056b1adba76
teacher <teacher@DESKTOP-TDDQARH>
2025-03-17 19:28:41 +0800

    j

---
+++ JavaOCP/src/day06/OO3.java
@@ -0,0 +1,17 @@
+package day06;
+
+public class OO3 {
+	public static void main(String[] args) {
+		Student s1 = new Student("John", 70);
+		Student s2 = new Student("Mary", 40);
+		Student s3 = new Student("Jack", 50);
+		Student s4 = new Student("Rose", 90);
+		Student s5 = new Student("Jean", 80);
+		
+		Student[] students = {s1, s2, s3, s4, s5};
+		
+		// 請印出及格的學生姓名與分數
+		
+		
+	}
+}
