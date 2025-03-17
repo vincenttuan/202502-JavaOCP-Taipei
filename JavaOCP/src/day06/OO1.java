47d5d52b8e40225dc7346c2dfacd97411da14993
teacher <teacher@DESKTOP-TDDQARH>
2025-03-17 19:00:43 +0800

    j

--- JavaOCP/src/day06/OO1.java
+++ JavaOCP/src/day06/OO1.java
@@ -7,10 +7,10 @@ public class OO1 {
 		s1.name = "John";
 		s1.score = 70;
 		
-		Student s2 = new Student();
-		s2.name = "Mary";
-		s2.score = 80;
+		Student s2 = new Student("Mary", 80);
 		
+		System.out.println(s1.name + ", " + s1.score);
+		System.out.println(s2.name + ", " + s2.score);
 		
 
 	}
--- JavaOCP/src/day06/Student.java
+++ JavaOCP/src/day06/Student.java
@@ -3,4 +3,17 @@ package day06;
 public class Student {
 	String name;
 	int score;
+	
+	// 建構子 1
+	public Student() {
+		System.out.println("建立 Student 物件");
+	}
+	
+	// 建構子 2
+	public Student(String name, int score) {
+		System.out.println("建立 Student 物件");
+		this.name = name;
+		this.score = score;
+	}
+	
 }
