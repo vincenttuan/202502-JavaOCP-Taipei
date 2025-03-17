c1d26d402e2c1d0f2f25043c4d7190e8d6454ee0
teacher <teacher@DESKTOP-TDDQARH>
2025-03-17 20:52:49 +0800

    j

---
+++ JavaOCP/src/day06/Product.java
@@ -0,0 +1,15 @@
+package day06;
+
+// 商品
+public class Product {
+	String name; // 品名
+	int qty; // 數量
+	int price; // 價格
+	
+	public Product(String name, int qty, int price) {
+		this.name = name;
+		this.qty = qty;
+		this.price = price;
+	}
+	
+}
