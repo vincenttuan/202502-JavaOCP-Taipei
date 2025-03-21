b611438198d28880164c4d9956a139c4d826af22
teacher <teacher@DESKTOP-TDDQARH>
2025-03-21 19:09:08 +0800

    j

---
+++ JavaOCP/src/day07/FastFoodStore.java
@@ -0,0 +1,19 @@
+package day07;
+
+// 快餐店
+public class FastFoodStore {
+
+	public static void main(String[] args) {
+		FastFood f1 = new FastFood("漢堡", 50);
+		FastFood f2 = new FastFood("炸雞", 60);
+		FastFood f3 = new FastFood("義大利麵", 75);
+		Drink d1 = new Drink("可樂", 20, false);
+		Drink d2 = new Drink("咖啡", 30, true);
+		Drink d3 = new Drink("紅茶", 25, false);
+		
+		
+		
+
+	}
+
+}
