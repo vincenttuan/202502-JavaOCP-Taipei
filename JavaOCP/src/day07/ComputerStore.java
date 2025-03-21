de3849ce730e122991a13a2f612519673cfac2b7
teacher <teacher@DESKTOP-TDDQARH>
2025-03-21 21:24:53 +0800

    j

---
+++ JavaOCP/src/day07/ComputerStore.java
@@ -0,0 +1,17 @@
+package day07;
+
+public class ComputerStore {
+
+	public static void main(String[] args) {
+		CPU cpu1 = new CPU("i7-15", 28000);
+		CPU cpu2 = new CPU("i5-01", 3000);
+		RAM ram1 = new RAM("4G", 800);
+		RAM ram2 = new RAM("32G", 2800);
+		HD hd1 = new HD("500G", 500);
+		HD hd2 = new HD("2TB", 2500);
+		// 幫我透過 Computer 組合一台"電競電腦" 與一台"文書電腦"
+		// 請問這二台電腦要花費多少錢 ?
+
+	}
+
+}
