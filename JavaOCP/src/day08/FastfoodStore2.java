12fd7f5c3de820a53e1b41febc4ebb818ccb61c2
teacher <teacher@DESKTOP-TDDQARH>
2025-03-24 19:34:44 +0800

    j

---
+++ JavaOCP/src/day08/FastfoodStore2.java
@@ -0,0 +1,11 @@
+package day08;
+
+public class FastfoodStore2 {
+	public static void main(String[] args) {
+		Hamburger hamburger = new Hamburger("大麥克", 70, 2);
+		Drink drink = new Drink("可樂", 20, false);
+		hamburger.printInfo();
+		drink.printInfo();
+		
+	}
+}
