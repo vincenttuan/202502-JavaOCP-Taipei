20c9c52db16031ca48184f0ed45507da1bfe2fa0
teacher <teacher@DESKTOP-TDDQARH>
2025-03-24 20:36:15 +0800

    j

---
+++ JavaOCP/src/day08/FastfoodStore3.java
@@ -0,0 +1,17 @@
+package day08;
+
+public class FastfoodStore3 {
+	public static void main(String[] args) {
+		Hamburger hamburger1 = new Hamburger("大麥克", 81, 2);
+		Hamburger hamburger2 = new Hamburger("麥香魚", 52, 1);
+		Hamburger hamburger3 = new Hamburger("嫩煎雞腿堡", 86, 1);
+		Drink drink1 = new Drink("可樂", 20, false);
+		Drink drink2 = new Drink("咖啡", 45, true);
+		Drink drink3 = new Drink("雪碧", 20, false);
+		//---------------------------------------------------
+		Hamburger[] hamburgers = {hamburger1, hamburger2, hamburger3};
+		Drink[] drinks = {drink1, drink2, drink3};
+		
+		
+	}
+}
