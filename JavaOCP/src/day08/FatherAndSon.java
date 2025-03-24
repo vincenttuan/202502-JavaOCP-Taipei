f3e04e6ba9de4bbb5df02be7fef7454a9e49b45d
teacher <teacher@DESKTOP-TDDQARH>
2025-03-24 18:51:35 +0800

    j

--- JavaOCP/src/day08/FatherAndSon.java
+++ JavaOCP/src/day08/FatherAndSon.java
@@ -3,8 +3,10 @@ package day08;
 public class FatherAndSon {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Father father = new Father();
+		System.out.println(father.balance);
+		father.play();
+		
 	}
 
 }
