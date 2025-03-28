32bf1f8ed0663a410b8e5d2dff2ef25140e197b1
teacher <teacher@DESKTOP-TDDQARH>
2025-03-28 19:08:53 +0800

    j

--- JavaOCP/src/day09/Main.java
+++ JavaOCP/src/day09/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		Dog dog = new Dog();
 		System.out.println(dog.getName());
+		
+		Cat cat = new Cat();
+		System.out.println(cat.getName());
 	}
 
 }
