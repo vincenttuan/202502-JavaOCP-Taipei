2b23d5352a3cf3577aa84cc4785b72f231e737e8
teacher <teacher@DESKTOP-TDDQARH>
2025-03-28 19:47:21 +0800

    j

---
+++ JavaOCP/src/day09/Main4.java
@@ -0,0 +1,15 @@
+package day09;
+
+public class Main4 {
+
+	public static void main(String[] args) {
+		Employee employee1 = new Employee(4_0000);
+		Employee employee2 = new Manager(6_0000, 3_0000);
+		Employee employee3 = new Supervistor(12_0000, 10_0000, 200_0000);
+		
+		Employee[] employees = {employee1, employee2, employee3};
+		// 請問薪資總和 = ?
+		
+	}
+
+}
