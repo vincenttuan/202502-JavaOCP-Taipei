67aa4ebc2df7dddc0e245ec7a5328412dbf45461
teacher <teacher@DESKTOP-TDDQARH>
2025-03-28 20:46:33 +0800

    j

---
+++ JavaOCP/src/p2/Son2.java
@@ -0,0 +1,12 @@
+package p2;
+
+import p1.Father;
+
+public class Son2 extends Father {
+	
+	public Son2() {
+		//System.out.println(super.banana);
+		System.out.println(super.cherry);
+		System.out.println(super.dragon);
+	}
+}
