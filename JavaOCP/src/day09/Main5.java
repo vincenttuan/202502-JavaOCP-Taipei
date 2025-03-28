1a2f5b3cd1a99a88721622d53e6e405b595679bd
teacher <teacher@DESKTOP-TDDQARH>
2025-03-28 20:34:09 +0800

    j

--- JavaOCP/src/day09/Main5.java
+++ JavaOCP/src/day09/Main5.java
@@ -4,8 +4,10 @@ public class Main5 {
 
 	public static void main(String[] args) {
 		Cat cat = new Cat();
+		Cat cat2 = new Tiger();
 		Tiger tiger = new Tiger();
 		cat.voice();
+		cat2.voice();
 		tiger.voice();
 	}
 
