44deb50a08fc8121fad9277c0e4f023d9a953de0
teacher <teacher@DESKTOP-TDDQARH>
2025-03-28 18:59:16 +0800

    j

--- JavaOCP/src/day09/Animal.java
+++ JavaOCP/src/day09/Animal.java
@@ -4,7 +4,7 @@ public class Animal {
 	private String name;
 	
 	public Animal() {
-		
+		System.out.println("Animal 建構子");
 	}
 	
 	public String getName() {
--- JavaOCP/src/day09/Dog.java
+++ JavaOCP/src/day09/Dog.java
@@ -3,7 +3,7 @@ package day09;
 public class Dog extends Animal {
 	
 	public Dog() {
-		
+		System.out.println("Dog 建構子");
 	}
 	
 }
