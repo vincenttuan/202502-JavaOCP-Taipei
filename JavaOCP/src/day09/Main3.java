7d119e414a5ff9b39e70ee498c9693f40a11fe2f
teacher <teacher@DESKTOP-TDDQARH>
2025-03-28 19:40:47 +0800

    j

--- JavaOCP/src/day09/Main3.java
+++ JavaOCP/src/day09/Main3.java
@@ -4,18 +4,13 @@ public class Main3 {
 
 	public static void main(String[] args) {
 		Employee employee = new Employee(4_0000);
-		Manager manager = new Manager(6_0000, 3_0000);
-		Supervistor supervistor = new Supervistor(12_0000, 10_0000, 200_0000);
+		Employee manager = new Manager(6_0000, 3_0000);
+		Employee supervistor = new Supervistor(12_0000, 10_0000, 200_0000);
 		
 		System.out.println(employee.getSalary());
-		
 		System.out.println(manager.getSalary());
-		System.out.println(manager.getBudget());
-		
 		System.out.println(supervistor.getSalary());
-		System.out.println(supervistor.getBudget());
-		System.out.println(supervistor.getStockOptions());
-
+		
 	}
 
 }
