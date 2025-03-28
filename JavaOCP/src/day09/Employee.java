81062b796c2a9a04495b12798441fcec3180d53a
teacher <teacher@DESKTOP-TDDQARH>
2025-03-28 19:24:23 +0800

    j

---
+++ JavaOCP/src/day09/Employee.java
@@ -0,0 +1,19 @@
+package day09;
+
+public class Employee {
+	private int salary;
+	
+	public Employee(int salary) {
+		this.salary = salary;
+	}  
+	
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+}
