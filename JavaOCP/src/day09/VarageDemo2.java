9438128404b767d878167d0184f3fee27d42a785
teacher <teacher@DESKTOP-TDDQARH>
2025-03-28 18:49:20 +0800

    j

---
+++ JavaOCP/src/day09/VarageDemo2.java
@@ -0,0 +1,21 @@
+package day09;
+
+public class VarageDemo2 {
+
+	public static void main(String[] args) {
+		System.out.println(calc(10, 20));
+		System.out.println(calc(10, 20, 30));
+		int[] values = {10, 20, 30, 40, 50};
+		System.out.println(calc(values));
+	}
+	
+	public static int calc(int... values) {
+		int sum = 0;
+		for(int i=0;i<values.length;i++) {
+			sum += values[i];
+		}
+		return sum;
+	}
+	
+	
+}
