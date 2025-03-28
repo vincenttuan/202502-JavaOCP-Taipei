64275049ea010d32b08f746dcb4a20c05f961ae9
teacher <teacher@DESKTOP-TDDQARH>
2025-03-28 20:47:25 +0800

    j

--- JavaOCP/src/p1/Father.java
+++ JavaOCP/src/p1/Father.java
@@ -2,7 +2,7 @@ package p1;
 
 public class Father {
 	private String apple = "蘋果";
-	String banana = "香蕉";
-	protected String cherry = "櫻桃";
+	String banana = "香蕉"; // 不可跨資料夾存取
+	protected String cherry = "櫻桃"; // 可以跨資料夾存取但是要有繼承關係
 	public String dragon = "火龍果";
 }
