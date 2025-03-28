bd6f58c4b9f8770ff8336a7d5f3aa2e2e31e2530
teacher <teacher@DESKTOP-TDDQARH>
2025-03-28 19:45:26 +0800

    j

--- JavaOCP/src/day09/Main3.java
+++ JavaOCP/src/day09/Main3.java
@@ -3,13 +3,18 @@ package day09;
 public class Main3 {
 
 	public static void main(String[] args) {
-		Employee employee = new Employee(4_0000);
-		Employee manager = new Manager(6_0000, 3_0000);
-		Employee supervistor = new Supervistor(12_0000, 10_0000, 200_0000);
+		Employee employee1 = new Employee(4_0000);
+		Employee employee2 = new Manager(6_0000, 3_0000);
+		Employee employee3 = new Supervistor(12_0000, 10_0000, 200_0000);
 		
-		System.out.println(employee.getSalary());
-		System.out.println(manager.getSalary());
-		System.out.println(supervistor.getSalary());
+		System.out.println(employee1.getSalary());
+		System.out.println(employee2.getSalary());
+		System.out.println(employee3.getSalary());
+		
+		System.out.println(((Manager)employee2).getBudget()); // 利用轉型
+		System.out.println(((Manager)employee3).getBudget());
+		
+		System.out.println(((Supervistor)employee3).getStockOptions());
 		
 	}
 
