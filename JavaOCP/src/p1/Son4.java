ae17987d26581af590fe55e9add73d2520be5031
teacher <teacher@DESKTOP-TDDQARH>
2025-03-28 20:53:04 +0800

    j

---
+++ JavaOCP/src/p1/Son4.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class Son4 {
+	public Son4() {
+		Father father = new Father();
+		System.out.println(father.banana);
+		System.out.println(father.cherry);
+		System.out.println(father.dragon);
+	}
+}
