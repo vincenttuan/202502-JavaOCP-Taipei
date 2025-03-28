411f482db39f04894855a43a283fc119a8d721fa
teacher <teacher@DESKTOP-TDDQARH>
2025-03-28 20:43:58 +0800

    j

---
+++ JavaOCP/src/p1/Son.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class Son extends Father {
+	
+	public Son() {
+		System.out.println(super.banana);
+		System.out.println(super.cherry);
+		System.out.println(super.dragon);
+	}
+}
