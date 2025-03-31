639e3632600ee221d47a7911176ab790a11a3975
teacher <teacher@DESKTOP-TDDQARH>
2025-03-31 20:29:54 +0800

    j

--- JavaOCP/src/day10/Coffee.java
+++ JavaOCP/src/day10/Coffee.java
@@ -1,28 +1,7 @@
 package day10;
 
 // 實現類
-public class Coffee implements Drink {
-	private String name; // 保存品名
-	private int price; // 保存價格
+public class Coffee extends DrinkSemi {
 	
-	@Override
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public int getPrice() {
-		return price;
-	}
-
-	@Override
-	public void setPrice(int price) {
-		this.price = price;
-	}
 	
 }
