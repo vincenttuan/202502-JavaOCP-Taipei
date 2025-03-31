3581e4ba8ae8c7f7b5d026dd19c7af1c2f48d6f5
teacher <teacher@DESKTOP-TDDQARH>
2025-03-31 21:24:57 +0800

    j

---
+++ JavaOCP/src/day10/CarTest.java
@@ -0,0 +1,14 @@
+package day10;
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		Car benz = new Benz("Benz", 300_0000);
+		Car bmw = new BMW("BMW", 250_0000);
+		Car honda = new Honda("Honda", 110_0000);
+		Car toyota = new Toyota("Toyota", 90_0000);
+		
+
+	}
+
+}
