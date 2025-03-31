f3ce75b3780d7e41ece0005566d17e54e8782e43
teacher <teacher@DESKTOP-TDDQARH>
2025-03-31 21:20:34 +0800

    j

--- JavaOCP/src/day10/CarSemi.java
+++ JavaOCP/src/day10/CarSemi.java
@@ -5,13 +5,19 @@ public abstract class CarSemi implements Car {
 	private String name;
 	private int price;
 	
+	// 建構子
+	public CarSemi(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
 	@Override
 	public String getName() {
 		return name;
 	}
 
 	@Override
-	public String getPrice() {
+	public int getPrice() {
 		return price;
 	}
 	
