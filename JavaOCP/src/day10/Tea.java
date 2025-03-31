40dbde3eaba43c9d8635d0f23a7fc984b4024823
teacher <teacher@DESKTOP-TDDQARH>
2025-03-31 19:55:42 +0800

    j

---
+++ JavaOCP/src/day10/Tea.java
@@ -0,0 +1,27 @@
+package day10;
+
+public class Tea implements Drink {
+	private String name;
+	private int price;
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int getPrice() {
+		return price;
+	}
+
+	@Override
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
