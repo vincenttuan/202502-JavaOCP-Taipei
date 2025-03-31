e06d1470643c485a66be73e702cfc8150c0622a8
teacher <teacher@DESKTOP-TDDQARH>
2025-03-31 21:27:44 +0800

    j

--- JavaOCP/src/day10/CarTest.java
+++ JavaOCP/src/day10/CarTest.java
@@ -8,6 +8,10 @@ public class CarTest {
 		Car honda = new Honda("Honda", 110_0000);
 		Car toyota = new Toyota("Toyota", 90_0000);
 		
+		Car[] cars = {benz, bmw, honda, toyota};
+		for(Car car : cars) {
+			System.out.printf("品牌 %-6s 價格 $%,9d%n", car.getName(), car.getPrice());
+		}
 
 	}
 
