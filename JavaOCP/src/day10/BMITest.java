178ac6048c0752f0835a482963642fe9067f2558
teacher <teacher@DESKTOP-TDDQARH>
2025-03-31 19:22:33 +0800

    j

--- JavaOCP/src/day10/BMITest.java
+++ JavaOCP/src/day10/BMITest.java
@@ -3,7 +3,8 @@ package day10;
 public class BMITest {
 
 	public static void main(String[] args) {
-		BMI bmi = new BMIA();
+		//BMI bmi = new BMIA();
+		BMI bmi = new BMIB();
 		double bmiValue = bmi.calcAndGet(170.0, 70.0);
 		System.out.println(bmiValue);
 		String result = bmi.getResult(bmiValue);
