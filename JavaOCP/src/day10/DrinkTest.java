e5c489de29131ca2285da22726034727ba7994a9
teacher <teacher@DESKTOP-TDDQARH>
2025-03-31 19:56:52 +0800

    j

--- JavaOCP/src/day10/DrinkTest.java
+++ JavaOCP/src/day10/DrinkTest.java
@@ -3,10 +3,15 @@ package day10;
 public class DrinkTest {
 
 	public static void main(String[] args) {
-		Drink drink = new Coffee();
-		drink.setName("拿鐵咖啡");
-		drink.setPrice(60);
-		System.out.printf("%s $%d%n", drink.getName(), drink.getPrice());
+		Drink coffee = new Coffee();
+		coffee.setName("拿鐵咖啡");
+		coffee.setPrice(60);
+		System.out.printf("%s $%d%n", coffee.getName(), coffee.getPrice());
+		
+		Drink tea = new Tea();
+		tea.setName("紅茶拿鐵");
+		tea.setPrice(37);
+		System.out.printf("%s $%d%n", tea.getName(), tea.getPrice());
 	}
 
 }
