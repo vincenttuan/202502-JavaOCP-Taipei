ab49e68395ef3cf910edaeae1de2019601a27543
teacher <teacher@DESKTOP-TDDQARH>
2025-04-07 19:45:30 +0800

    j

---
+++ JavaOCP/src/day11/AbstractProduct.java
@@ -0,0 +1,26 @@
+package day11;
+
+// 抽象商品類別
+public class AbstractProduct implements Product {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
---
+++ JavaOCP/src/day11/Product.java
@@ -0,0 +1,8 @@
+package day11;
+
+// 商品介面規格
+public interface Product {
+	public String getName(); // 商品名稱
+	public int getPrice(); // 商品價格
+	public void display(); // 顯示商品資訊
+}
