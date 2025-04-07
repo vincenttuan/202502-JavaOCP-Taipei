ea93b308e99526cbc628e2dbb1d0a1068bf9ea88
teacher <teacher@DESKTOP-TDDQARH>
2025-04-07 20:47:23 +0800

    j

---
+++ JavaOCP/src/day11/ShoppingCart2.java
@@ -0,0 +1,19 @@
+package day11;
+
+public class ShoppingCart2 {
+
+	public static void main(String[] args) {
+		Product p1 = new PhoneProduct("iPhone-16", 65000, "Apple");
+		Product p2 = new PhoneProduct("Android-17", 45000, "Google");
+		Product p3 = new ClothingProduct("Sweater", 800, "L");
+		Product p4 = new ClothingProduct("T-Shirt", 500, "M");
+		
+		// 已購買的商品 
+		Product[] products = {p1, p2, p3, p4};
+		
+		// 請計算出結帳金額(不折扣) = ?
+		
+		
+	}
+
+}
