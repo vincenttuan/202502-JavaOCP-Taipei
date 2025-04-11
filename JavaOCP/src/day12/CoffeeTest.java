aa99be7feb7980df489780d79e2388654871500d
teacher <teacher@DESKTOP-TDDQARH>
2025-04-11 19:56:55 +0800

    j

--- JavaOCP/src/day12/CoffeeTest.java
+++ JavaOCP/src/day12/CoffeeTest.java
@@ -3,9 +3,14 @@ package day12;
 public class CoffeeTest {
 
 	public static void main(String[] args) {
-		Coffee coffee = new Coffee("Java", 100, 3);
+		Coffee coffee = new Coffee("Java豆", 100, 3);
 		coffee.describe();
-
+		System.out.println("------------");
+		Coffee.Bean bean = new Coffee().new Bean("巴西豆");
+		bean.describe();
+		System.out.println("------------");
+		Coffee.Milk milk = new Coffee().new Milk(250);
+		milk.describe();
 	}
 
 }
