611dc32bdee8821422cc04b447978d6cf54978bb
teacher <teacher@DESKTOP-TDDQARH>
2025-04-11 18:50:42 +0800

    j

--- JavaOCP/src/day11/ShoppingCart3.java
+++ JavaOCP/src/day11/ShoppingCart3.java
@@ -21,16 +21,31 @@ public class ShoppingCart3 {
 		// 請計算出結帳金額(折扣) = ?
 		int total2 = Stream.of(products)
 						   .peek(p -> ((AbstractProduct)p).applyDiscount())
-						   .mapToInt(p -> p.getPrice()).sum();;
+						   .mapToInt(p -> p.getPrice()).sum();
 		System.out.printf("結帳金額(折扣):$%,d%n", total2);
 		
 		// 請計算出結帳金額(買手機有折扣,買衣服原價) = ?
-		//int total3 = 0;
-		//System.out.printf("結帳金額(買手機有折扣,買衣服原價):$%,d%n", total3);
+		int total3 = Stream.of(products)
+				   .peek(p -> {
+					   if(p instanceof PhoneProduct) {
+						   ((PhoneProduct)p).applyDiscount();
+					   }
+				   })
+				   .mapToInt(p -> p.getPrice()).sum();
+		System.out.printf("結帳金額(買手機有折扣,買衣服原價):$%,d%n", total3);
 		
 		// 請計算出結帳金額(買 Apple 手機才有折扣,其他原價) = ?
-		//int total4 = 0;
-		//System.out.printf("結帳金額(買 Apple 手機才有折扣,其他原價):$%,d%n", total4);
+		int total4 = Stream.of(products)
+				   .peek(p -> {
+					   if(p instanceof PhoneProduct) {
+						   PhoneProduct phoneProduct = ((PhoneProduct)p); 
+						   if(phoneProduct.getBrand().equals("Apple")) {
+							   phoneProduct.applyDiscount();
+						   }
+					   }
+				   })
+				   .mapToInt(p -> p.getPrice()).sum();
+		System.out.printf("結帳金額(買 Apple 手機才有折扣,其他原價):$%,d%n", total4);
 		
 	}
 
