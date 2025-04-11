a4c4f5b32026ec51a6f4613a821ad291d9869910
teacher <teacher@DESKTOP-TDDQARH>
2025-04-11 20:53:27 +0800

    j

--- JavaOCP/src/day12/Car.java
+++ JavaOCP/src/day12/Car.java
@@ -4,17 +4,32 @@ package day12;
 public class Car {
 	
 	// 啟動引擎
-	public void startEngine() {
-		System.out.println("啟動");
+	public void startEngine1() {
+		System.out.println("啟動1");
 		// 引擎-方法內部類別
 		class Engine {
 			void run() {
-				System.out.println("引擎運轉");
+				System.out.println("4缸引擎運轉");
 			}
 		}
 		// 建立引擎物件
 		Engine engine = new Engine();
 		engine.run();
 	}
+	// 啟動引擎
+	public void startEngine2() {
+		System.out.println("啟動2");
+		// 引擎-方法內部類別
+		class Engine {
+			void run() {
+				System.out.println("2缸引擎運轉");
+			}
+		}
+		// 建立引擎物件
+		Engine engine = new Engine();
+		engine.run();	
+	}
+	
+	
 	
 }
--- JavaOCP/src/day12/CarTest.java
+++ JavaOCP/src/day12/CarTest.java
@@ -4,7 +4,8 @@ public class CarTest {
 
 	public static void main(String[] args) {
 		Car car = new Car();
-		car.startEngine();
+		car.startEngine1();
+		car.startEngine2();
 	}
 
 }
