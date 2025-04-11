85ad14f44fef2eea86830b0982ea6a206f8cb99a
teacher <teacher@DESKTOP-TDDQARH>
2025-04-11 21:07:00 +0800

    j

--- JavaOCP/src/day12/Outer2.java
+++ JavaOCP/src/day12/Outer2.java
@@ -3,11 +3,11 @@ package day12;
 public class Outer2 {
 	
 	public void describe() {
-		int x = 10;
+		int x = 10; // 區域變數
 		
 		class Inner2 {
 			void show() {
-				System.out.println(x);
+				System.out.println(x); // 區域變數 x 必須是 final 區域變數
 			}
 		}
 		
