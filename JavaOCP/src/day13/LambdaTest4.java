10645a7abc2b1ad53eb6af6c9f5a859781d30347
teacher <teacher@DESKTOP-TDDQARH>
2025-04-18 20:34:52 +0800

    j

--- JavaOCP/src/day13/LambdaTest4.java
+++ JavaOCP/src/day13/LambdaTest4.java
@@ -1,16 +1,20 @@
 package day13;
 
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class LambdaTest4 {
 
 	public static void main(String[] args) {
-		// Consumer<T> 應用
+		// Consumer<T> 
 		Consumer<String> consumer = (t) -> System.out.println("程式語言:" + t);
-		
+		// 應用 1
 		consumer.accept("Java");
 		consumer.accept("C#");
-		
+		System.out.println("-----------");
+		// 應用 2
+		Stream.of("Java", "C#", "VB", "Python")
+			  .forEach(consumer);
 
 	}
 
