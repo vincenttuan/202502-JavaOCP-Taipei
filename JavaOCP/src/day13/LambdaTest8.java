b4aeb863d06e3d6bbd4d6fc5ee5edb132c6ff69b
teacher <teacher@DESKTOP-TDDQARH>
2025-04-18 21:02:38 +0800

    j

---
+++ JavaOCP/src/day13/LambdaTest8.java
@@ -0,0 +1,9 @@
+package day13;
+
+public class LambdaTest8 {
+	public static void main(String[] args) {
+		String[] productNames = {
+				"iPhone-15(Hot)", "iPad-Air", "Google Pixel Pro(Hot)", "iPhone-16", "Samsung Galaxy", "AirPods Pro(Hot)"
+		};
+	}
+}
