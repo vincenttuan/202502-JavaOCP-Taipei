2f262a67b7a0fb3162cd0bd7ee7a7e6a51e8f022
teacher <teacher@DESKTOP-TDDQARH>
2025-04-18 19:22:40 +0800

    j

---
+++ JavaOCP/src/day13/LambdaTest3.java
@@ -0,0 +1,19 @@
+package day13;
+
+// 過濾商品是否有含特殊字串
+interface NameFilter {
+	boolean test(String productName, String keywordName);
+}
+
+public class LambdaTest3 {
+
+	public static void main(String[] args) {
+		String[] productNames = {
+				"iPhone-15", "iPad-Air", "Google Pixel Pro", "iPhone-16", "Samsung Galaxy", "AirPods Pro"
+		};
+		// 請利用函數是程式設計過濾出 iPhone 手機有哪些? 
+		
+
+	}
+
+}
