ba3199fc282c1bc7fdbf4d9c8575cf9ff3c152ec
teacher <teacher@DESKTOP-TDDQARH>
2025-04-18 21:25:32 +0800

    j

--- JavaOCP/src/day13/LambdaTest8.java
+++ JavaOCP/src/day13/LambdaTest8.java
@@ -1,5 +1,10 @@
 package day13;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 public class LambdaTest8 {
 	public static void main(String[] args) {
 		String[] productNames = {
@@ -7,11 +12,19 @@ public class LambdaTest8 {
 				"iPhone-16", "Samsung Galaxy", "AirPods Pro(Hot)"
 		};
 		// 1. 利用 Predicate 篩選出熱門商品 (Hot)
+		Predicate<String> filterHot = (name) -> name.contains("Hot");
 		
 		// 2. 利用 Function 轉換名稱加上 🔥 圖樣
+		Function<String, String> mapName = (name) -> "🔥 " + name;
 		
 		// 3. 利用 Consumer 印出熱門商品
+		Consumer<String> printName = (name) -> System.out.println(name);
 		
+		// 整合
+		Stream.of(productNames)
+			  .filter(filterHot)
+			  .map(mapName)
+			  .forEach(printName);
 		
 	}
 }
