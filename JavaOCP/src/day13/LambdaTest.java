6f4d4f46ae0a4793bf5c5227ee5c578144013ad5
teacher <teacher@DESKTOP-TDDQARH>
2025-04-18 19:03:55 +0800

    j

--- JavaOCP/src/day13/LambdaTest.java
+++ JavaOCP/src/day13/LambdaTest.java
@@ -15,7 +15,11 @@ public class LambdaTest {
 		Check checkPass = (score) -> score >= 60;
 		// 判斷不及格
 		Check checkNonPass = (score) -> score < 60;
-		
+		// 判斷高標
+		Check checkHighPass = (score) -> score >= 80;
+		// 判斷低標
+		Check checkLowPass = (score) -> score <= 40;
+				
 		// 過濾出及格分數並逐一輸出
 		IntStream.of(scores)
 				 .filter(score -> checkPass.test(score))
@@ -24,6 +28,16 @@ public class LambdaTest {
 		// 過濾出不及格分數並逐一輸出
 		IntStream.of(scores)
 				 .filter(score -> checkNonPass.test(score))
+				 .forEach(score -> System.out.println(score));
+		System.out.println("----");
+		// 過濾出高標分數並逐一輸出
+		IntStream.of(scores)
+				 .filter(score -> checkHighPass.test(score))
+				 .forEach(score -> System.out.println(score));
+		System.out.println("----");
+		// 過濾出低標分數並逐一輸出
+		IntStream.of(scores)
+				 .filter(score -> checkLowPass.test(score))
 				 .forEach(score -> System.out.println(score));
 				
 	}
