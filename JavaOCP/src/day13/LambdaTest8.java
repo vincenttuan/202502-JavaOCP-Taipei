bce0f4ff1c5ab864367088e1d6015d600efd9ada
teacher <teacher@DESKTOP-TDDQARH>
2025-04-18 21:05:14 +0800

    j

--- JavaOCP/src/day13/LambdaTest8.java
+++ JavaOCP/src/day13/LambdaTest8.java
@@ -3,7 +3,15 @@ package day13;
 public class LambdaTest8 {
 	public static void main(String[] args) {
 		String[] productNames = {
-				"iPhone-15(Hot)", "iPad-Air", "Google Pixel Pro(Hot)", "iPhone-16", "Samsung Galaxy", "AirPods Pro(Hot)"
+				"iPhone-15(Hot)", "iPad-Air", "Google Pixel Pro(Hot)", 
+				"iPhone-16", "Samsung Galaxy", "AirPods Pro(Hot)"
 		};
+		// 1. 利用 Predicate 篩選出熱門商品 (Hot)
+		
+		// 2. 利用 Function 轉換名稱加上 🔥 圖樣
+		
+		// 3. 利用 Consumer 印出熱門商品
+		
+		
 	}
 }
