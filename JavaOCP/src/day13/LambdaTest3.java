5c65d348d001b009fbbff6fd94ad65627f5261b9
teacher <teacher@DESKTOP-TDDQARH>
2025-04-18 19:41:44 +0800

    j

--- JavaOCP/src/day13/LambdaTest3.java
+++ JavaOCP/src/day13/LambdaTest3.java
@@ -1,5 +1,7 @@
 package day13;
 
+import java.util.stream.Stream;
+
 // 過濾商品是否有含特殊字串
 interface NameFilter {
 	boolean test(String productName, String keywordName);
@@ -11,9 +13,23 @@ public class LambdaTest3 {
 		String[] productNames = {
 				"iPhone-15", "iPad-Air", "Google Pixel Pro", "iPhone-16", "Samsung Galaxy", "AirPods Pro"
 		};
-		// 請利用函數是程式設計過濾出 iPhone 手機有哪些? 
-		
+		// 請利用函數是程式設計過濾出 iPhone 手機有哪些?
+		NameFilter nameFilter = (productName, keywordName) -> productName.contains(keywordName);
+		filterProductName(productNames, nameFilter, "iPhone");
 
+	}
+	
+	public static void filterProductName(String[] productNames, NameFilter nameFilter, String keywordName) {
+		/*
+		for(String productName : productNames) {
+			if(nameFilter.test(productName, keywordName)) {
+				System.out.println(productName);
+			}
+		}
+		*/
+		Stream.of(productNames)
+			  .filter(productName -> nameFilter.test(productName, keywordName))
+			  .forEach(productName -> System.out.println(productName));
 	}
 
 }
