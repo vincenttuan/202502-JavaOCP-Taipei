58a387614376a6842ed53ebc249251efbfdb354a
teacher <teacher@DESKTOP-TDDQARH>
2025-04-21 21:34:56 +0800

    j

--- JavaOCP/src/day14/SetDemo3.java
+++ JavaOCP/src/day14/SetDemo3.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class SetDemo2 {
+public class SetDemo3 {
 
 	public static void main(String[] args) {
 		Set<String> set = new HashSet<>();
@@ -15,15 +15,10 @@ public class SetDemo2 {
 		set.add("數");
 		System.out.println(set);
 		// 如何移除 null 與 100 元素 ?
-		Iterator<String> iter = set.iterator();
-		while(iter.hasNext()) {
-			String data = iter.next();
-			if(data == null || data.equals("100")) {
-				iter.remove();
-			}
-		}
-		System.out.println(set);
+		// 利用 removeIf
+		set.removeIf(data -> data == null || data.equals("100"));
 		
+		System.out.println(set);
 	}
 
 }
