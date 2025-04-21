7035a4ca558c29add635d09a27568208bd2d7a7e
teacher <teacher@DESKTOP-TDDQARH>
2025-04-21 19:48:06 +0800

    j

---
+++ JavaOCP/src/day14/PaymentMethod.java
@@ -0,0 +1,10 @@
+package day14;
+
+// 傳統版
+public interface PaymentMethod {
+	
+	public static final String CASH = "現金支付";
+	public static final String CREDIT_CARD = "信用卡支付";
+	public static final String MOBILE_PAY = "手機支付";
+	
+}
