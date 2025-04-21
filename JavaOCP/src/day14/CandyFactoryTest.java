7ccec2990ccf99046d9ac44edde7c302f330b3ea
teacher <teacher@DESKTOP-TDDQARH>
2025-04-21 18:58:41 +0800

    j

--- JavaOCP/src/day14/CandyFactoryTest.java
+++ JavaOCP/src/day14/CandyFactoryTest.java
@@ -5,6 +5,9 @@ public class CandyFactoryTest {
 	public static void main(String[] args) {
 		CandyFactory.Candy candy = new CandyFactory.Candy("草莓口味跳跳糖");
 		candy.display();
+		
+		CandyFactory.Machine machine = new CandyFactory().new Machine();
+		machine.display();
 	}
 
 }
