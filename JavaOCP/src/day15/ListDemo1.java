5c31028fe5637cadc7640f1ebe6589eae23722ae
teacher <teacher@DESKTOP-TDDQARH>
2025-04-25 20:54:02 +0800

    j

--- JavaOCP/src/day15/ListDemo1.java
+++ JavaOCP/src/day15/ListDemo1.java
@@ -10,12 +10,19 @@ public class ListDemo1 {
 		scores.add(95);
 		scores.add(88);
 		scores.add(100);
-		scores.add(1, 45);
+		scores.add(45);
 		scores.add(66);
 		scores.add(100);
 		System.out.println(scores);
 		System.out.println(scores.get(0)); // 第一筆
 		System.out.println(scores.get(scores.size()-1)); // 最末筆
+		// 插入指定位置
+		scores.add(4, 77);
+		System.out.println(scores);
+		scores.remove(1); // 刪除1位置(88)的資料
+		System.out.println(scores);
+		scores.remove(Integer.valueOf(45)); // 刪除內容物是45的資料
+		System.out.println(scores);
 		
 		
 		
