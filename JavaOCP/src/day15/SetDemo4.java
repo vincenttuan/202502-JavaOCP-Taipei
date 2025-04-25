62791b9999d2743157b137a156ca09693a9f3a37
teacher <teacher@DESKTOP-TDDQARH>
2025-04-25 18:58:07 +0800

    j

--- JavaOCP/src/day15/SetDemo4.java
+++ JavaOCP/src/day15/SetDemo4.java
@@ -1,13 +1,14 @@
 package day15;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.jar.Attributes.Name;
 
 public class SetDemo4 {
 
 	public static void main(String[] args) {
-		// HashSet 不在乎順序, 只求快
+		// HashSet: 不在乎順序, 只求快
 		// 使用情境: 快速查詢來賓是否有參加活動 party
 		Set<String> party = new HashSet<>();
 		party.add("小明");
@@ -22,7 +23,23 @@ public class SetDemo4 {
 		} else {
 			System.out.printf("%s沒有報名%n", user);
 		}
-
+		
+		// LinkedHashSet: 保留加入的順序
+		// 使用情境: 記錄使用者最近購買的商品
+		Set<String> shopping = new LinkedHashSet<>();
+		shopping.add("iPhone");
+		shopping.add("AirPods");
+		shopping.add("iPad");
+		//shopping.add("iPhone"); // 重複不能加入
+		System.out.println("您最近點過的商品:");
+		//shopping.forEach(p -> System.out.println(p));
+		shopping.forEach(System.out::println); // :: 方法參考
+		
+		
+		
+		
+		
+		
 	}
 
 }
