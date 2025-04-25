842bc20471fb3e25a8f569261c614819d867cf1c
teacher <teacher@DESKTOP-TDDQARH>
2025-04-25 21:15:28 +0800

    j

---
+++ JavaOCP/src/day15/Observer.java
@@ -0,0 +1,6 @@
+package day15;
+
+// 觀察者(讀者)
+public interface Observer {
+	void update(String message);
+}
--- JavaOCP/src/scores/ListDemo2.java
+++
@@ -1,9 +0,0 @@
-package scores;
-
-public class ListDemo2 {
-	public static void main(String[] args) {
-		// 堆疊 stack 與 佇列 Queue
-		
-		
-	}
-}
