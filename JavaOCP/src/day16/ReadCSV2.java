efc3930c82a4a695c0e864babc6b3fb3205d55d0
teacher <teacher@DESKTOP-TDDQARH>
2025-04-28 19:55:00 +0800

    j

--- JavaOCP/src/day16/ReadCSV2.java
+++ JavaOCP/src/day16/ReadCSV2.java
@@ -2,8 +2,9 @@ package day16;
 
 public class ReadCSV2 {
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		// 你有一個 CSV 檔案（例如 100m.csv），其中包含了多位選手的姓名與他們在 100 公尺賽跑中的多次成績。
+		// 你需要利用 Sportsman 這個 JavaBean 類別來讀取檔案並計算每一位選手的平均成績，並將結果列印出來。
 
 	}
 
