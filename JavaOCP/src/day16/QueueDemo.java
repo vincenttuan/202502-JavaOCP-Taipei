cb321c6287322548d78c84322e67ecfcc3fdafde
teacher <teacher@DESKTOP-TDDQARH>
2025-04-28 18:53:09 +0800

    j

--- JavaOCP/src/day16/QueueDemo.java
+++ JavaOCP/src/day16/QueueDemo.java
@@ -14,7 +14,11 @@ public class QueueDemo {
 		queue.offer("Charlie");
 		System.out.println("目前排隊的人:" + queue);
 		
-		
+		// 模擬買票
+		while (!queue.isEmpty()) { // 查看是否還有排隊的人
+			String person = queue.poll(); // 取出對首的人
+			System.out.println(person + "買了票, 目前排隊的人:" + queue);
+		}
 		
 	}
 }
