dbb0c601ed25b5fe1fb3d81097a5ffe9bc05bc9c
teacher <teacher@DESKTOP-TDDQARH>
2025-05-02 18:57:08 +0800

    j

---
+++ JavaOCP/src/day17/InputScore.java
@@ -0,0 +1,20 @@
+package day17;
+
+import java.util.Scanner;
+
+public class InputScore {
+
+	public static void main(String[] args) {
+		// 使用者輸入成績之後系統判斷是否及格
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("請輸入成績:");
+		int score = scanner.nextInt();
+		if(score >= 60) {
+			System.out.println("及格");
+		} else {
+			System.out.println("不及格");
+		}
+		System.out.println("程式結束!");
+	}
+
+}
