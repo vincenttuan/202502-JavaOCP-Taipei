aa06fe7cd1513bf4d02f13b49cbe9c9e7a772be5
teacher <teacher@DESKTOP-TDDQARH>
2025-05-02 19:01:30 +0800

    j

--- JavaOCP/src/day17/InputScore.java
+++ JavaOCP/src/day17/InputScore.java
@@ -1,5 +1,6 @@
 package day17;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputScore {
@@ -8,13 +9,19 @@ public class InputScore {
 		// 使用者輸入成績之後系統判斷是否及格
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("請輸入成績:");
-		int score = scanner.nextInt();
-		if(score >= 60) {
-			System.out.println("及格");
-		} else {
-			System.out.println("不及格");
+		try {
+			int score = scanner.nextInt();
+			if(score >= 60) {
+				System.out.println("及格");
+			} else {
+				System.out.println("不及格");
+			}
+			System.out.println("程式結束!");
+		} catch (InputMismatchException e) {
+			System.out.println("輸入資料不正確, 請重新輸入 !");
+			System.out.println(e);
 		}
-		System.out.println("程式結束!");
+		
 	}
 
 }
