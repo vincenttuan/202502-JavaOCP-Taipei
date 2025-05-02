a50cf3647aecd2e55b9045ca0b0f60acea24e579
teacher <teacher@DESKTOP-TDDQARH>
2025-05-02 19:15:29 +0800

    j

--- JavaOCP/src/day17/BMICalculator.java
+++ JavaOCP/src/day17/BMICalculator.java
@@ -5,6 +5,7 @@ public class BMICalculator {
 	public static void main(String[] args) {
 		// 請撰寫一支 Java 程式，讓使用者從鍵盤輸入自己的「身高（公分）」與「體重（公斤）」，
 		// 並計算出 BMI 值
+		// 注意:當使用者輸入身高正確後，如果體重輸入錯誤，只需要重新輸入體重，不必重新輸入身高。
 
 	}
 
