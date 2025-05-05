06c1909120338feb5e39fd1e4e3034af52edfa6b
teacher <teacher@DESKTOP-TDDQARH>
2025-05-05 21:02:47 +0800

    j

---
+++ JavaOCP/src/day18/ReadStudent.java
@@ -0,0 +1,20 @@
+package day18;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ReadStudent {
+
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei&characterEncoding=utf-8&useUnicode=true&useSSL=false";
+		//----------------------
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		
+
+	}
+
+}
