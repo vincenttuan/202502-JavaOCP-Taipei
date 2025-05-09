9eae733176be2313766ddd802b651eeb7b41e4f9
teacher <teacher@DESKTOP-TDDQARH>
2025-05-09 20:49:21 +0800

    j

---
+++ JavaOCP/src/day19/ReadAccountBalance.java
@@ -0,0 +1,12 @@
+package day19;
+
+public class ReadAccountBalance {
+
+	public static void main(String[] args) {
+		// 查詢每一個帳戶的餘額
+		// 並建立 AccountBalance 物件來存放
+		// 1.印出每一個人的帳戶餘額
+		// 2.印出帳戶總餘額
+	}
+
+}
