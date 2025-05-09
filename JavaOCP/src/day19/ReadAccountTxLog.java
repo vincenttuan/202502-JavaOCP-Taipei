ee1094b806350a4876c3f2db1a66ec02d80aa096
teacher <teacher@DESKTOP-TDDQARH>
2025-05-09 19:58:41 +0800

    j

--- JavaOCP/src/day19/ReadAccountTxLog.java
+++ JavaOCP/src/day19/ReadAccountTxLog.java
@@ -2,6 +2,7 @@ package day19;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -16,14 +17,21 @@ public class ReadAccountTxLog {
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(sql)) {
 			
+			// 標題列
+			ResultSetMetaData md = rs.getMetaData();
+			System.out.println("+----------+----------+----------+----------+");
+			System.out.printf("|%-10s|%-10s|%-10s|%-10s|%n", 
+					md.getColumnName(1), md.getColumnName(2), md.getColumnName(3), md.getColumnName(4));
+			System.out.println("+----------+----------+----------+----------+");
+			// 內容
 			while(rs.next()) {
 				Integer id = rs.getInt("id");
 				String name = rs.getString("name");
 				String txType = rs.getString("tx_type");
 				Integer amount = rs.getInt("amount");
-				System.out.printf("%-10d%-10s%-10s%,10d%n", id, name, txType, amount);
+				System.out.printf("|%-10d|%-10s|%-10s|%,10d|%n", id, name, txType, amount);
 			}
-			
+			System.out.println("+----------+----------+----------+----------+");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
