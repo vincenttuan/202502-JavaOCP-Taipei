b3ef951136c3f694744e68d5d0d49ef5228dff66
teacher <teacher@DESKTOP-TDDQARH>
2025-05-09 21:20:28 +0800

    j

--- JavaOCP/src/day19/ReadAccountBalance.java
+++ JavaOCP/src/day19/ReadAccountBalance.java
@@ -1,5 +1,12 @@
 package day19;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadAccountBalance {
 
 	public static void main(String[] args) {
@@ -7,6 +14,35 @@ public class ReadAccountBalance {
 		// 並建立 AccountBalance 物件來存放
 		// 1.印出每一個人的帳戶餘額
 		// 2.印出帳戶總餘額
+	}
+	
+	
+	public static List<AccountBalance> finAccountBalances() {
+		List<AccountBalance> list = new ArrayList<>();
+		String sql = "select a.id, a.`name`, coalesce(sum(case when t.tx_type = 'deposit' then t.amount "
+				+ "when t.tx_type = 'withdraw' then -t.amount "
+				+ "else 0 end), 0) as balance "
+				+ "from account a "
+				+ "left join tx_log t on a.id = t.account_id "
+				+ "group by a.id, a.name ";
+		
+		try(Connection conn = MySQLConnection.getConnection();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql)) {
+			
+			while(rs.next()) {
+				Integer id = rs.getInt("id");
+				String name = rs.getString("name");
+				Integer balance = rs.getInt("balance");
+				AccountBalance accountBalance = new AccountBalance(id, name, balance);
+				list.add(accountBalance);
+			}
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return list;
 	}
 
 }
