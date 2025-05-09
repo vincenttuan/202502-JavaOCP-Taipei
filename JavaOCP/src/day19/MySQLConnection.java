9617f4f39ca2276d8d7474ce51ccc3372efd3302
teacher <teacher@DESKTOP-TDDQARH>
2025-05-09 19:46:44 +0800

    j

--- JavaOCP/src/day19/MySQLConnection.java
+++ JavaOCP/src/day19/MySQLConnection.java
@@ -6,15 +6,19 @@ import java.sql.DriverManager;
 public class MySQLConnection {
 	private static Connection conn;
 	
-	static {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String jdbcUrl = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei&characterEncoding=utf-8&useUnicode=true&useSSL=false";
-			String username = "root";
-			String password = "12345678";
-			conn = DriverManager.getConnection(jdbcUrl, username, password);
-		} catch (Exception e) {
-			e.printStackTrace();
+	public static Connection getConnection() {
+		if(conn == null) {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				String jdbcUrl = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei&characterEncoding=utf-8&useUnicode=true&useSSL=false";
+				String username = "root";
+				String password = "12345678";
+				conn = DriverManager.getConnection(jdbcUrl, username, password);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
+		return conn;
 	}
+	
 }
--- JavaOCP/src/day19/ReadAccountTxLog.java
+++ JavaOCP/src/day19/ReadAccountTxLog.java
@@ -1,10 +1,32 @@
 package day19;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class ReadAccountTxLog {
 
 	public static void main(String[] args) {
 		// 查詢每一個帳戶的交易紀錄
-
+		String sql = "select a.id, a.`name`, t.tx_type, t.amount "
+					 + "from account a "
+					 + "left join tx_log t on a.id = t.account_id";
+		try(Connection conn = MySQLConnection.getConnection();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql)) {
+			
+			while(rs.next()) {
+				Integer id = rs.getInt("id");
+				String name = rs.getString("name");
+				String txType = rs.getString("tx_type");
+				Integer amount = rs.getInt("amount");
+				System.out.printf("%d\t%s\t%s\t%d%n", id, name, txType, amount);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
