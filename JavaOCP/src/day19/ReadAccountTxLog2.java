3107e1050fe1712e731a45d1ddfa71c65843cf85
teacher <teacher@DESKTOP-TDDQARH>
2025-05-09 20:36:54 +0800

    j

--- JavaOCP/src/day19/ReadAccountTxLog2.java
+++ JavaOCP/src/day19/ReadAccountTxLog2.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ReadAccountTxLog2 {
@@ -25,7 +26,17 @@ public class ReadAccountTxLog2 {
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(sql)) {
 			
-			
+			while(rs.next()) {
+				Integer id = rs.getInt("id");
+				String name = rs.getString("name");
+				String txType = rs.getString("tx_type");
+				Integer amount = rs.getInt("amount");
+				Date txDate = rs.getDate("tx_date");
+				// 建立 AccountTxLog 物件
+				AccountTxLog aTxLog = new AccountTxLog(id, name, txType, amount, txDate);
+				// 注入到 list 集合中保存
+				list.add(aTxLog);
+			}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
