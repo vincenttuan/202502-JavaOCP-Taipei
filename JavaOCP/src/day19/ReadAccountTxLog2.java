9008471e983cff29a8b54e80a0b6cd0b3934e5c4
teacher <teacher@DESKTOP-TDDQARH>
2025-05-09 20:34:50 +0800

    j

--- JavaOCP/src/day19/ReadAccountTxLog2.java
+++ JavaOCP/src/day19/ReadAccountTxLog2.java
@@ -1,10 +1,37 @@
 package day19;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadAccountTxLog2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	}
+	
+	public static List<AccountTxLog> findAccountTxLogs() {
+		List<AccountTxLog> list = new ArrayList<>();
+		
+		// 查詢每一個帳戶的交易紀錄
+		String sql = "select a.id, a.`name`, t.tx_type, t.amount, t.tx_date "
+					 + "from account a "
+					 + "left join tx_log t on a.id = t.account_id";
+		
+		try(Connection conn = MySQLConnection.getConnection();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql)) {
+			
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return list;
 	}
 
 }
