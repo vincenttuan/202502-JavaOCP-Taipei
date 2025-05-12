802d1fa9d48d82ff7a32ba364cf187fe205d795a
teacher <teacher@DESKTOP-TDDQARH>
2025-05-12 18:51:16 +0800

    j

--- JavaOCP/src/day20/Task.java
+++ JavaOCP/src/day20/Task.java
@@ -6,7 +6,7 @@ public class Task {
 		// 取得執行緒名稱:
 		String tname = Thread.currentThread().getName();
 		// 執行 1~1000 的迴圈
-		for(int i=1;i<=1000;i++) {
+		for(int i=1;i<=1000;i+=2) {
 			System.out.printf("%s 執行: %d%n", tname, i);
 		}
 	}
--- JavaOCP/src/day20/TaskMain.java
+++ JavaOCP/src/day20/TaskMain.java
@@ -5,6 +5,7 @@ public class TaskMain {
 	public static void main(String[] args) {
 		Task task = new Task();
 		task.job();
+		
 	}
 	
 }
