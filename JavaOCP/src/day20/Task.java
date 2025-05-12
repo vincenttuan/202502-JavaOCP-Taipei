01a43b2f68f7ecd167977b8350fa10ff6f5aac0c
teacher <teacher@DESKTOP-TDDQARH>
2025-05-12 18:48:12 +0800

    j

---
+++ JavaOCP/src/day20/Task.java
@@ -0,0 +1,14 @@
+package day20;
+
+public class Task {
+	
+	public void job() {
+		// 取得執行緒名稱:
+		String tname = Thread.currentThread().getName();
+		// 執行 1~1000 的迴圈
+		for(int i=1;i<=1000;i++) {
+			System.out.printf("%s 執行: %d%n", tname, i);
+		}
+	}
+	
+}
