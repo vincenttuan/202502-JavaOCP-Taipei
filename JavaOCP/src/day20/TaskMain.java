f41c80b5e8e86d4664e9df72fb8d16365c295226
teacher <teacher@DESKTOP-TDDQARH>
2025-05-12 19:19:24 +0800

    j

--- JavaOCP/src/day20/TaskMain.java
+++ JavaOCP/src/day20/TaskMain.java
@@ -5,10 +5,15 @@ public class TaskMain {
 	public static void main(String[] args) {
 		TaskOdd taskOdd = new TaskOdd();
 		TaskEven taskEven = new TaskEven();
+		// 單工模式
+		//taskOdd.job();
+		//taskEven.job();
 		
-		taskOdd.job();
-		taskEven.job();
-		
+		// 多執行緒模式
+		Thread t1 = new Thread(new TaskOddRunnable(taskOdd));
+		Thread t2 = new Thread(new TaskEventRunnable(taskEven));
+		t1.start();
+		t2.start();
 	}
 	
 }
