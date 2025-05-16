53bb6e789e241a6dbb8077430eb29e8b0cde2b55
teacher <teacher@DESKTOP-TDDQARH>
2025-05-16 21:24:35 +0800

    j

--- JavaOCP/src/day21/YoubikeDemo2.java
+++ JavaOCP/src/day21/YoubikeDemo2.java
@@ -13,7 +13,7 @@ public class YoubikeDemo2 {
 		for(Youbike y : youbikes) {
 			PositionParam param2 = new PositionParam(y.getLatitude()+"", y.getLongitude()+"");
 			double m = GPSUtil.getDistanceByCoordinate(param2, param1);
-			if(y.getAvailable_rent_bikes() >= 20) {
+			if(y.getAvailable_rent_bikes() >= 50) {
 				count++;
 				System.out.printf("距離巨匠東認: %,.1fm 借/還: %d/%d 台 %s %s (%f, %f)%n", 
 						m, y.getAvailable_rent_bikes(), y.getAvailable_return_bikes(), y.getSna(), y.getAr(), y.getLatitude(), y.getLongitude());
