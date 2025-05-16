220e1fcc56b826d50790ad71c7065ac1dde1f8cd
teacher <teacher@DESKTOP-TDDQARH>
2025-05-16 21:23:37 +0800

    j

---
+++ JavaOCP/src/day21/YoubikeDemo2.java
@@ -0,0 +1,25 @@
+package day21;
+
+import java.util.List;
+
+public class YoubikeDemo2 {
+
+	public static void main(String[] args) throws Exception {
+		// 巨匠東認  25.041809, 121.550414
+		PositionParam param1 = new PositionParam("25.041809", "121.550414");
+		
+		List<Youbike> youbikes = YoubikeUtil.getYoubikes();
+		int count = 0;
+		for(Youbike y : youbikes) {
+			PositionParam param2 = new PositionParam(y.getLatitude()+"", y.getLongitude()+"");
+			double m = GPSUtil.getDistanceByCoordinate(param2, param1);
+			if(y.getAvailable_rent_bikes() >= 20) {
+				count++;
+				System.out.printf("距離巨匠東認: %,.1fm 借/還: %d/%d 台 %s %s (%f, %f)%n", 
+						m, y.getAvailable_rent_bikes(), y.getAvailable_return_bikes(), y.getSna(), y.getAr(), y.getLatitude(), y.getLongitude());
+			}
+		}
+		System.out.printf("資料筆數: %d 筆%n", count);
+	}
+
+}
