08e8d23c543530d3b8059373ebd59dc96ccd77ec
teacher <teacher@DESKTOP-TDDQARH>
2025-05-16 21:07:39 +0800

    j

---
+++ JavaOCP/src/day21/GPSUtil.java
@@ -0,0 +1,46 @@
+package day21;
+
+import lombok.Data;
+
+@Data
+class PositionParam {
+    private String positionlng;  //坐标经度
+    private String positionlat;  //坐标纬度
+
+}
+
+public class GPSUtil {
+
+
+    // 圆周率
+    private static final Double PI = Math.PI;
+    private static final Double PK = 180 / PI;
+    private static final Double EARTH_RADII = 6370996.81;
+
+
+    /**
+     * 计算两个百度地图坐标实际距离：米
+     *
+     * @param sourcePosition
+     * @param targetPosition
+     * @return
+     */
+    public static double getDistanceByCoordinate(PositionParam sourcePosition, PositionParam targetPosition) {
+        double o_lat = Double.valueOf(sourcePosition.getPositionlat());
+        double o_lng = Double.valueOf(sourcePosition.getPositionlng());
+
+        double d_lat = Double.valueOf(targetPosition.getPositionlat());
+        double d_lng = Double.valueOf(targetPosition.getPositionlng());
+
+        double t1 = Math.cos(o_lat / PK) * Math.cos(o_lng / PK) * Math.cos(d_lat / PK) * Math.cos(d_lng / PK);
+
+        double t2 = Math.cos(o_lat / PK) * Math.sin(o_lng / PK) * Math.cos(d_lat / PK) * Math.sin(d_lng / PK);
+
+        double t3 = Math.sin(o_lat / PK) * Math.sin(d_lat / PK);
+
+        double tt = Math.acos(t1 + t2 + t3);
+
+        return EARTH_RADII * tt;
+    }
+
+}
--- JavaOCP/src/day21/YoubikeDemo.java
+++ JavaOCP/src/day21/YoubikeDemo.java
@@ -1,10 +1,15 @@
 package day21;
 
+import java.util.List;
+
 public class YoubikeDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// 巨匠東認  25.041809, 121.550414
-		
+		List<Youbike> youbikes = YoubikeUtil.getYoubikes();
+		for(Youbike y : youbikes) {
+			System.out.printf("%s (%f, %f)%n", y.getSna(), y.getLatitude(), y.getLongitude());
+		}
 	}
 
 }
