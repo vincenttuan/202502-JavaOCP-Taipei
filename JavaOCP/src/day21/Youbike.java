d426f738150444b515e61e05177dfe932fe8e3f4
teacher <teacher@DESKTOP-TDDQARH>
2025-05-16 18:56:07 +0800

    j

---
+++ JavaOCP/src/day21/Youbike.java
@@ -0,0 +1,27 @@
+/* Copyright 2025 freecodeformat.com */
+package day21;
+
+import java.util.Date;
+
+public class Youbike {
+    private String sno;
+    private String sna;
+    private String sarea;
+    private String mday;
+    private String ar;
+    private String sareaen;
+    private String snaen;
+    private String aren;
+    private String act;
+    private String srcupdatetime;
+    private String updatetime;
+    private String infotime;
+    private Date infodate;
+    private int total;
+    private int availableRentBikes;
+    private double latitude;
+    private double longitude;
+    private int availableReturnBikes;
+    
+
+}
