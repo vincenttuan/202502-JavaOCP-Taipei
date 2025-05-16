5a7b7213429e183bd17697e72bf4b52b13d85153
teacher <teacher@DESKTOP-TDDQARH>
2025-05-16 20:34:10 +0800

    j

--- JavaOCP/src/day21/YoubikeMain.java
+++ JavaOCP/src/day21/YoubikeMain.java
@@ -5,6 +5,7 @@ public class YoubikeMain {
 	public static void main(String[] args) {
 		// 每 30 秒執行一次
 		Runnable runnable = () -> {
+			System.out.println("Youbike 排程啟動(每 30 秒執行乙次):");
 			while(true) {
 				try {
 					Thread.sleep(30_000); // 延遲 30 秒
