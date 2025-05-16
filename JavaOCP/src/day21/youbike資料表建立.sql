8c745463466ac5fdabb3abca6678aa70347318c9
teacher <teacher@DESKTOP-TDDQARH>
2025-05-16 19:58:19 +0800

    j

--- JavaOCP/src/day21/youbike資料表建立.sql
+++ JavaOCP/src/day21/youbike資料表建立.sql
@@ -9,10 +9,10 @@ create table if not exists youbike (
 	snaen varchar(100), -- 站名(英文)
 	aren varchar(200), -- 地址(英文)
 	act varchar(10), -- 是否啟用(1/0)
-	srcupdatetime varchar(30), -- 原始資料更新時間
-	updatetime varchar(30), -- 資料處理時間
-	infotime varchar(30), -- 資訊時間
-	infodate date, -- 資訊日期
+	srcUpdateTime varchar(30), -- 原始資料更新時間
+	updateTime varchar(30), -- 資料處理時間
+	infoTime varchar(30), -- 資訊時間
+	infoDate date, -- 資訊日期
 	total int, -- 全部車數
 	available_rent_bikes int, -- 可借車數
 	latitude double, -- 緯度
