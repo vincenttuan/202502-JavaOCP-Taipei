27ae976d344a35099b225e85f817b0f201ac8362
teacher <teacher@DESKTOP-TDDQARH>
2025-05-19 19:22:54 +0800

    j

---
+++ JavaOCP/src/day22/JoinDemo.java
@@ -0,0 +1,35 @@
+package day22;
+
+import java.util.Random;
+
+public class JoinDemo {
+	static int high = 0;
+	static int low = 0;
+	
+	
+	public static void main(String[] args) {
+		Runnable r1 = () -> {
+			Random random = new Random();
+			high = random.nextInt(20) + 81; // 高標
+			System.out.println("高標:" + high);
+		};
+		Runnable r2 = () -> {
+			Random random = new Random();
+			low = random.nextInt(20) + 31; // 低標
+			System.out.println("低標:" + low);
+		};
+		
+		Thread t1 = new Thread(r1); // 計算高標執行緒
+		Thread t2 = new Thread(r2); // 計算低標執行緒
+		System.out.println("t1 啟動");
+		t1.start();
+		System.out.println("t2 啟動");
+		t2.start();
+		
+		// 計算均標
+		double avg = (high + low)/2.0;
+		System.out.printf("均標:%.1f%n", avg);
+		
+	}
+
+}
