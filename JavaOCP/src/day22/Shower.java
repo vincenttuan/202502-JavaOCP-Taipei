36928f2fdaba3afe0a350f5ba99117b6eb0854c2
teacher <teacher@DESKTOP-TDDQARH>
2025-05-19 19:07:13 +0800

    j

---
+++ JavaOCP/src/day22/Shower.java
@@ -0,0 +1,40 @@
+package day22;
+
+class Father implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("爸爸下班回家");
+		System.out.println("爸爸準備洗澡");
+		System.out.println("爸爸發現沒瓦斯了");
+		System.out.println("爸爸打電話叫瓦斯");
+		// -------------------------------------------
+		Thread worder = new Thread(new Worker());
+		worder.start(); // 叫瓦斯工人送瓦斯
+		// -------------------------------------------
+		System.out.println("爸爸開始洗熱水澡");
+		System.out.println("爸爸洗完澡了");
+	}
+	
+}
+
+class Worker implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("工人送瓦斯");
+		System.out.println("工人在路上了");
+		System.out.println("工人送瓦斯來了");
+		System.out.println("工人將瓦斯安裝完畢了");
+	}
+	
+}
+
+public class Shower {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
