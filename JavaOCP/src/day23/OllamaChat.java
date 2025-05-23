3d4a48d9413bfccd04a76858f387a900fa0011fa
teacher <teacher@DESKTOP-TDDQARH>
2025-05-23 19:47:06 +0800

    j

--- JavaOCP/src/day23/OllamaChat.java
+++ JavaOCP/src/day23/OllamaChat.java
@@ -3,6 +3,7 @@ package day23;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class OllamaChat {
 
@@ -22,13 +23,21 @@ public class OllamaChat {
 		input = String.format(input, model, prompt);
 		System.out.println(input);
 		
+		System.out.println("傳送資料...");
 		HttpClient client = HttpClient.newHttpClient();
 		
+		// 請求 request
 		HttpRequest request = HttpRequest.newBuilder()
 				.uri(URI.create(path))
 				.header("Content-Type", "application/json")
 				.POST(HttpRequest.BodyPublishers.ofString(input))
 				.build();
+		
+		// 回應 response
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		
+		String result = response.body();
+		System.out.println(result);
 		
 	}
 
