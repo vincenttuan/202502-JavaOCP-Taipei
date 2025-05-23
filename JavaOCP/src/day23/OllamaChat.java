3de5e91ec2198167f6051127c60a24a28e31bf2b
teacher <teacher@DESKTOP-TDDQARH>
2025-05-23 19:44:40 +0800

    j

--- JavaOCP/src/day23/OllamaChat.java
+++ JavaOCP/src/day23/OllamaChat.java
@@ -1,8 +1,13 @@
 package day23;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class OllamaChat {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		String path = "http://localhost:11434/api/chat";
 		String model = "qwen2.5:0.5b";
 		String prompt = "你是誰";
 		String input = "{"
@@ -16,7 +21,15 @@ public class OllamaChat {
 				+ "}";
 		input = String.format(input, model, prompt);
 		System.out.println(input);
-
+		
+		HttpClient client = HttpClient.newHttpClient();
+		
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create(path))
+				.header("Content-Type", "application/json")
+				.POST(HttpRequest.BodyPublishers.ofString(input))
+				.build();
+		
 	}
 
 }
