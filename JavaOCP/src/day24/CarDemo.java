b9a117d418d7a15464c3b35b3a1b57ea524ac663
teacher <teacher@DESKTOP-TDDQARH>
2025-05-26 21:25:34 +0800

    j

--- JavaOCP/src/day24/CarDemo.java
+++ JavaOCP/src/day24/CarDemo.java
@@ -3,14 +3,16 @@ package day24;
 import java.util.concurrent.CyclicBarrier;
 
 public class CarDemo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Runnable lunch = () -> System.out.println("吃午餐囉~~");
-		int n = 3;
+		Runnable dinner = () -> System.out.println("吃晚餐囉~~");
+		int n = 2;
 		
 		CyclicBarrier cyclicBarrier = new CyclicBarrier(n, lunch);
 		new Thread(new Car(cyclicBarrier), "A車").start();
 		new Thread(new Car(cyclicBarrier), "B車").start();
 		new Thread(new Car(cyclicBarrier), "C車").start();
+		new Thread(new Car(cyclicBarrier), "D車").start();
 		
 	}
 }
