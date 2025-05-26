fb53fe5dbb7612cc5eb16b903f2213836da07d3f
teacher <teacher@DESKTOP-TDDQARH>
2025-05-26 20:34:13 +0800

    j

--- JavaOCP/src/day24/AILottery.java
+++ JavaOCP/src/day24/AILottery.java
@@ -8,6 +8,8 @@ public class AILottery {
 	public static void main(String[] args) throws Exception {
 		 String lotteryHistory = getLotteryHistory();
 		 System.out.printf("539樂透歷史資料: %s%n", lotteryHistory);
+		 // 請透過 ollama 來預測下一期的539樂透開獎號碼
+		 // 539是1~39取5個不會重複的號碼
 	}
 	
 	private static String getLotteryHistory() throws Exception {
