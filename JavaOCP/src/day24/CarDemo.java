9a0d140d58354e349897c0ae957754450fc0a7d2
teacher <teacher@DESKTOP-TDDQARH>
2025-05-26 21:16:29 +0800

    j

---
+++ JavaOCP/src/day24/CarDemo.java
@@ -0,0 +1,16 @@
+package day24;
+
+import java.util.concurrent.CyclicBarrier;
+
+public class CarDemo {
+	public static void main(String[] args) {
+		Runnable lunch = () -> System.out.println("吃午餐囉~~");
+		int n = 3;
+		
+		CyclicBarrier cyclicBarrier = new CyclicBarrier(n);
+		new Thread(new Car(cyclicBarrier), "A車").start();
+		new Thread(new Car(cyclicBarrier), "B車").start();
+		new Thread(new Car(cyclicBarrier), "C車").start();
+		
+	}
+}
