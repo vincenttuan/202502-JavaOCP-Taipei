37194e61d93f87a2957272f4a7f54e89111c154c
teacher <teacher@DESKTOP-TDDQARH>
2025-05-26 21:17:07 +0800

    j

--- JavaOCP/src/day24/CarDemo.java
+++ JavaOCP/src/day24/CarDemo.java
@@ -7,7 +7,7 @@ public class CarDemo {
 		Runnable lunch = () -> System.out.println("吃午餐囉~~");
 		int n = 3;
 		
-		CyclicBarrier cyclicBarrier = new CyclicBarrier(n);
+		CyclicBarrier cyclicBarrier = new CyclicBarrier(n, lunch);
 		new Thread(new Car(cyclicBarrier), "A車").start();
 		new Thread(new Car(cyclicBarrier), "B車").start();
 		new Thread(new Car(cyclicBarrier), "C車").start();
