7c3185d71da4a8cf1877d3bffee7614618882951
teacher <teacher@DESKTOP-TDDQARH>
2025-06-02 19:38:55 +0800

    j

--- JavaOCP/src/day25/BMIJFrame.java
+++ JavaOCP/src/day25/BMIJFrame.java
@@ -27,8 +27,22 @@ public class BMIJFrame extends JFrame {
 		
 		// 設定版面配置 FlowLayout (元件會依序排在每一個元件的後面, 若視窗空間不足才會換行)
 		setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
-		// 新增元件
 		
+		// 建立元件
+		jLabelHeight = new JLabel("身高(cm):");
+		jLabelWeight = new JLabel("體重(kg):");
+		jLabelResult = new JLabel("BMI:");
+		jTextFieldHeight = new JTextField(5); // 5個字的空間
+		jTextFieldWeight = new JTextField(5); // 5個字的空間
+		jButtonCalc = new JButton("計算");
+		
+		// 配置元件到 JFrame 畫面上
+		add(jLabelHeight);     // 身高標籤
+		add(jTextFieldHeight); // 身高輸入框
+		add(jLabelWeight);     // 體重標籤輸入框
+		add(jTextFieldWeight); // 體重輸入框
+		add(jButtonCalc);      // 計算按鈕
+		add(jLabelResult);     // 計算標籤結果
 		
 	}
 	
