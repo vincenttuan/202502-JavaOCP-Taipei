6a9b2e286716bbf300aac8e59851aaf39fc3b02f
teacher <teacher@DESKTOP-TDDQARH>
2025-06-02 19:33:06 +0800

    j

--- JavaOCP/src/day25/BMIJFrame.java
+++ JavaOCP/src/day25/BMIJFrame.java
@@ -1,9 +1,18 @@
 package day25;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class BMIJFrame extends JFrame {
+	// 定義要用的 UI 元件
+	private JLabel jLabelHeight, jLabelWeight, jLabelResult; 
+	private JTextField jTextFieldHeight, jTextFieldWeight;
+	private JButton jButtonCalc;
 	
 	public BMIJFrame() {
 		init();
@@ -14,7 +23,13 @@ public class BMIJFrame extends JFrame {
 		setTitle("BMI 計算機");
 		setSize(300, 123); // 設定視窗大小(寬度, 高度)
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 按下視窗上方的"X"可以結束程式
-		setLocationRelativeTo(null); // 置中
+		setLocationRelativeTo(null); // 視窗置中
+		
+		// 設定版面配置 FlowLayout (元件會依序排在每一個元件的後面, 若視窗空間不足才會換行)
+		setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+		// 新增元件
+		
+		
 	}
 	
 	// 主程式
