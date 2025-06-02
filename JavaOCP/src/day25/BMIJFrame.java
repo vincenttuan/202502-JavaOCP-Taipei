b4f7f68d4d02bb2e3512858378652471c778c756
teacher <teacher@DESKTOP-TDDQARH>
2025-06-02 19:24:43 +0800

    j

--- JavaOCP/src/day25/BMIJFrame.java
+++ JavaOCP/src/day25/BMIJFrame.java
@@ -1,6 +1,7 @@
 package day25;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class BMIJFrame extends JFrame {
 	
@@ -13,6 +14,12 @@ public class BMIJFrame extends JFrame {
 		setTitle("BMI 計算機");
 		setSize(300, 123); // 設定視窗大小(寬度, 高度)
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 按下視窗上方的"X"可以結束程式
+	}
+	
+	// 主程式
+	public static void main(String[] args) {
+		// 啟動視窗
+		SwingUtilities.invokeLater(() -> new BMIJFrame().setVisible(true));
 	}
 	
 }
